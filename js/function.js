ccdb0a01396c6bd9b34c0fd30b19d6e7c70724bb
Cristian Reynals <crisigreynals@gmail.com>
2023-02-27 17:09:48 -0300

    Primer escritura en el repositorio

---
+++ js/function.js
@@ -0,0 +1,11 @@
+
+document.getElementById('boton').onclick = function () {
+    document.getElementById("fecha").innerHTML = "10/7/1950"
+    document.getElementById("domicilio").innerHTML = "6134 Dogwood Ave"
+    document.getElementById("mail").innerHTML = "brian.morales@example.com"
+}
+
+document.getElementById('boton_color').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#7BB39B';
+});
+
